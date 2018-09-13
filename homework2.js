3691a8391827fde350d8f910235a2c3dfdf1d5e4
aramazatyan5017 <azatyan.aram@gmail.com>
2018-09-13 19:11:53 +0400

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,2 +1,32 @@
-alert("wazzzzup");
+alert("Hey there!");
 
+const num1 = 15;
+const num2 = 13;
+const num3 = num1 * num2 - 100;
+
+const str1 = "15";
+const str2 = "Heyooo";
+const str3 = str1 + str2;
+
+const arr = [
+	{string1: "You there?",
+	string2: "15"}, 
+	{string1: "I'm here.",
+	string2: "Heyo"}, 
+	[13, 15, num3]];
+
+if(num2 < num1 && str1 === arr[0].string2) {
+	alert("Wazzzzzup?");	
+} else if(str3 === "heyooo15"){
+	alert("Seriously?");
+} else {
+	alert("Something's wrong.");
+}
+
+if(arr[1].string2 === str2 || arr[2][1] !== num2){
+	alert("Welcome to my HTML page!!!");
+} else{
+	alert("my name is uvuvwevwevwe onyetenyevwe ugwemubwem ossas");
+}
+
+console.log("Hey, should you even be here?");
